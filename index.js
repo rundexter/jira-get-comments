eccdc8aa2dc876d9771435f9d4fd9599ab95437e
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-10 13:46:48 +0200

    fix

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ module.exports = {
 
             if (_.isUndefined(outputValueByKey)) {
 
-                result = undefined;
+                result = _.isEmpty(result)? undefined : result;
                 return;
             }
 
